f365f292930d80c11bafe33489d0ef4a841814b4
Mark <mso872@gmail.com>
2020-08-24 13:33:29 -0700

    Import HomePage, NavBar components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import logo from './logo.svg';
-import './App.css';
-// import Button from '@material-ui/core/Button';
-import NameTable from "./components/nameTable";
+// import "./App.css";
+import HomePage from './components/HomePage'
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App" style={{display: 'flex'}}>
-      <NameTable />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+            <h2>This is a website to help choose an Armenian name</h2>
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2> ; 
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
