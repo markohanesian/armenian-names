7a4e37e188c355cdb2023b33f7205f84b333850d
markohanesian <mso872@gmail.com>
2020-09-13 10:39:50 -0700

    updated HomeTitle

---
+++ src/components/HomeTitle/HomeTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css'
+
+class HomeTitle extends React.Component {
+    render() {
+        return <h1>Armenian Names</h1>
+    }
+}
+
+export default HomeTitle
