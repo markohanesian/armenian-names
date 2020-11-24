cee005811538d8331922f56c2dccb2d216f7e09a
markohanesian <mso872@gmail.com>
2020-11-24 07:09:41 -0800

    Rearranged nav bar

--- src/App.js
+++ src/App.js
@@ -4,34 +4,44 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import logo from './logo.svg';
 import "./App.css";
 // COMPONENTS
-import HomeTitle from './components/HomeTitle/HomeTitle';
-import Subtitle from './components/Subtitle/Subtitle';
-import RandomName from './components/RandomName/RandomName';
-import GenArmFemaleName from './components/GenArmFemaleName/GenArmFemaleName';
-import GenArmMaleName from './components/GenArmMaleName/GenArmMaleName';
-
+import HomeTitle from "./components/HomeTitle/HomeTitle";
+import Subtitle from "./components/Subtitle/Subtitle";
+import RandomName from "./components/RandomName/RandomName";
+import GenArmFemaleName from "./components/GenArmFemaleName/GenArmFemaleName";
+import GenArmMaleName from "./components/GenArmMaleName/GenArmMaleName";
 
 export default function App() {
   return (
     // these are the nav contents
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Randomizer</Link>
-            </li>
-            <li>
-              <Link to="/bygender">By Gender</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            {/* <li>
+        <section>
+          <Route path="/">
+            <>
+              <HomeTitle />
+              <Subtitle />
+              <RandomName />
+            </>
+          </Route>
+        </section>
+        <section id="bottom-navigation-group">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Randomizer</Link>
+              </li>
+              <li>
+                <Link to="/bygender">By Gender</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              {/* <li>
               <Link to="/login">Login</Link>
             </li> */}
-          </ul>
-        </nav>
+            </ul>
+          </nav>
+        </section>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -47,23 +57,21 @@ export default function App() {
             </>
           </Route>
           <Route path="/about">
-            <div style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "1rem"}} >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                padding: "1rem",
+              }}
+            >
               <br></br>
               <p>This is a website to help choose an Armenian name</p>
               <p>Created by Mark Ohanesian, 2020</p>
               <a href="github.com/markohanesian">GitHub</a>
             </div>
-            
           </Route>
-          <Route path="/login">
-          </Route>
-          <Route path="/">
-            <>
-              <HomeTitle />
-              <Subtitle />
-              <RandomName />
-            </>
-          </Route>
+          <Route path="/login"></Route>
         </Switch>
       </div>
     </Router>
