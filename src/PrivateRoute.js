f73b5d51909b799c6e21ae3207703bc64b9166d6
markohanesian <mso872@gmail.com>
2020-12-10 11:31:45 -0800

    Formatting

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -4,22 +4,21 @@ import { AuthContext } from "./Auth";
 
 // specifies which component to render when user is Authenticated
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
-    // This hook accepts AuthContext from Auth
-    const { currentUser } = useContext(AuthContext);
-    return (
-        <Route
-            {...rest}
-            render={routeProps =>
-                // If we have user, render <RouteComponent />, else route <Redirect />
-                !!currentUser ? (
-                    <RouteComponent {...routeProps} />
-                ) : (
-                        <Redirect to={"/login"} />
-                    )
-            }
-        />
-    );
+  // This hook accepts AuthContext from Auth
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) =>
+        // If we have user, render <RouteComponent />, else route <Redirect />
+        !!currentUser ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+          <Redirect to={"/login"} />
+        )
+      }
+    />
+  );
 };
 
-
-export default PrivateRoute
+export default PrivateRoute;
