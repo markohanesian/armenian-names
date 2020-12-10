771ab8fce256a22651b9bfa7a5429a484a7e1f91
markohanesian <mso872@gmail.com>
2020-12-10 10:27:10 -0800

    Added new functional signup form page

---
+++ src/components/SignUpNew.js
@@ -0,0 +1,41 @@
+import React, { useCallback } from "react";
+import { withRouter } from "react-router";
+import app from "../base";
+
+const SignUp = ({ history }) => {
+    const handleSignUp = useCallback(async event => {
+        // this event will prevent the page from reloading
+        event.preventDefault();
+        const { email, password } = event.target.elements;
+        try {
+            await app
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+            // upon successful authentication, reroute user to root path
+            history.push("/");
+        } catch (error) {
+            // if authentication doesn't work, the error will alert
+            alert(error);
+        }
+    }, [history]);
+
+    return (
+        <div>
+            <h1>Sign up</h1>
+            {/* handleSignUp function passed into form  */}
+            <form onSubmit={handleSignUp}>
+                <label>
+                    Email
+          <input name="email" type="email" placeholder="Email" />
+                </label>
+                <label>
+                    Password
+          <input name="password" type="password" placeholder="Password" />
+                </label>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+
+export default withRouter(SignUp);
