e3de8cdb95b6a5e809b3f8b9e38f94d8c141dff9
markohanesian <mso872@gmail.com>
2021-01-05 11:22:16 -0800

    Updated signup page styling

--- src/components/SignUpNew.js
+++ src/components/SignUpNew.js
@@ -1,5 +1,7 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router";
+import { Grid, Button, TextField, Typography } from "@material-ui/core";
+// import GTranslateIcon from '@material-ui/icons/GTranslate';
 import app from "../base";
 
 // history object passed in as props
@@ -24,10 +26,9 @@ const SignUp = ({ history }) => {
   );
 
   return (
-    <div>
-      <h1>Sign up</h1>
+    <div Grid container class="grid-default" spacing={1}>
       {/* handleSignUp function passed into form  */}
-      <form onSubmit={handleSignUp}>
+      {/* <form onSubmit={handleSignUp}>
         <label>
           Email
           <input name="email" type="email" placeholder="Email" />
@@ -37,7 +38,41 @@ const SignUp = ({ history }) => {
           <input name="password" type="password" placeholder="Password" />
         </label>
         <button type="submit">Sign Up</button>
-      </form>
+      </form> */}
+      <Grid class="grid-default" item xs={12}>
+        <Typography variant="h5" gutterBottom>
+          Sign Up
+        </Typography>
+      </Grid>
+      <Grid className="grid-default">
+        <form onSubmit={handleSignUp}>
+          <Grid id="login-container" container justify="center">
+            <TextField label="Username" margin="normal" name="email" type="email"/>
+            <TextField label="Password" margin="normal" name="password" type="password"/>
+            {/* <TextField label="Repeat Password" margin="normal" name="password" type="password"/> */}
+            <Button type="submit" color="primary">
+                Create Account
+            </Button>
+            <Grid className="grid-row">
+                <Typography variant="caption" display="block" gutterBottom>
+                    Already have an account?
+                </Typography>
+                <Button href="#text-buttons" color="primary" type="submit">
+                    Login
+                </Button>
+            </Grid>
+            {/* <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                startIcon={<GTranslateIcon />}
+            >
+                Login with Google
+            </Button> */}
+          </Grid>
+          </form>
+
+      </Grid>
     </div>
   );
 };
